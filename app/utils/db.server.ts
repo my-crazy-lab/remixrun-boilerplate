eda87af21ebc22f7f1edbf2fc35d2ce27566313c
MinhLeeBtaskee <leminh.nguyen@btaskee.com>
2024-04-09 14:33:23 +0700

    test: ci

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -9,14 +9,6 @@ if (!connectionString) {
     'No connection string provided. \n\nPlease create a `.env` file in the root of this project. Add a CONNECTION_STRING variable to that file with the connection string to your MongoDB cluster. \nRefer to the README.md file for more information.',
   );
 }
-if (connectionString.indexOf('appName') === -1)
-  connectionString +=
-    connectionString.indexOf('?') > -1 ? '&appName=remix|' : '?appName=remix|';
-else
-  connectionString = connectionString.replace(
-    /appName\=([a-z0-9]*)/i,
-    (m, p) => `appName=remix|${p}`,
-  );
 
 let mongodb: Db;
 
