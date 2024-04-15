d59dc7b1e614baea31491f48200cebde398ae11e
minhthi.nguyen <minhthi.nguyen@btaskee.com>
2024-04-15 17:08:01 +0700

    ci: fix pipeline failed

--- app/services/role-base-access-control.server.ts
+++ app/services/role-base-access-control.server.ts
@@ -113,11 +113,7 @@ export async function createGroup({
 
   const parentGroup = await groupCol.findOne({ _id: parent });
   if (!parentGroup) {
-    toast({
-      variant: 'error',
-      title: 'ERROR',
-      description: 'PARENT_GROUP_NOT_FOUND',
-    });
+    throw new Error('PARENT_GROUP_NOT_FOUND')
   }
 
   await groupCol.insertOne({
@@ -237,8 +233,8 @@ export async function getGroupDetail<T = Document>({
     const matchParent = root
       ? {}
       : {
-          'parents.userIds': userId,
-        };
+        'parents.userIds': userId,
+      };
 
     const group = await mongodb
       .collection('groups')
